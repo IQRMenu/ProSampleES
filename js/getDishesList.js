8b5ec39c6296de16ace6c3cd27170d5dba0e8f70
IQRMenu <interactivemenuqr@gmail.com>
2025-02-24 23:47:24 -0300

    v5

--- js/getDishesList.js
+++ js/getDishesList.js
@@ -1,21 +1,19 @@
 // https://script.google.com/macros/s/AKfycbwmM5d62aYv1p_ybR5i8l7lmHmUlxBZ2BrhZGnrYcqHOvbXJobGRp801BPgbdo_o5OV/exec - Pranzo
 // https://script.google.com/macros/s/AKfycbzebC0Dsz3W8Gmsq_FDbr0QDcsusV-pGn5PxkL1FtGUGPO7q0aFebuJH1SFYzak9qs2Gw/exec - Sample
-export function fetchDishesList() {
-  return fetch('https://script.google.com/macros/s/AKfycbwmM5d62aYv1p_ybR5i8l7lmHmUlxBZ2BrhZGnrYcqHOvbXJobGRp801BPgbdo_o5OV/exec')
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Сеть ответила с ошибкой: ' + response.status);
-      }
-      // Преобразуем ответ в JSON
-      return response.json();
-    })
-    .then(data => {
-      return processData(data);
-    })
-    .catch(error => {
-      console.error('Ошибка:', error);
-      return [];
-    });
+export async  function fetchDishesList() {
+  const sheetId = "1luJmbsWfP-CtN16VV4grYt2MuAH1iRwVhnu6qez4EcI"; // ID твоей таблицы
+  const apiKey = "AIzaSyAoqUP1XTXL7Y5zXyQ9rfEgMy4d30qDC-Q"; // Вставь сюда API-ключ
+  const url = `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/Sheet1?key=${apiKey}`;
+
+  try {
+    let response = await fetch(url);
+    let data = await response.json();
+    
+    return processData(data.values); // Конвертируем в удобный формат
+  } catch (error) {
+    console.error("Ошибка загрузки данных:", error);
+    return [];
+  }
 }
 
 
@@ -30,3 +28,4 @@ function processData(data) {
   });
   return objectsArray;
 }
+
--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import { fetchDishesList } from "./getDishesList.js";
 import { words } from './words.js';
 import { globalData } from "./globalData.js";
-import { main } from "https://iqrmenu.github.io/mainScript/IQRMenuMain.js";
+import { main } from "https://iqrmenu.github.io/mainScript/IQRMenuMain.js"; //в продакшен открыть 
+// import { main } from "./IQRMenuMain.js"; //в тестовой открыть
 main(fetchDishesList, words, globalData);
